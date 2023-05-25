29856fb1c055f9d6e5147b7f96387ee81ac85df5
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2023-05-25 14:09:47 +0200

    Init

---
+++ frontend/src/pages/Event.js
@@ -0,0 +1,5 @@
+const EventPage = () => {
+  return <h1>The Event Page</h1>;
+};
+
+export default EventPage;
---
+++ frontend/src/pages/EventDetails.js
@@ -0,0 +1,5 @@
+const EventDetailsPage = () => {
+  return <h1>Event Details Page</h1>;
+};
+
+export default EventDetailsPage;
